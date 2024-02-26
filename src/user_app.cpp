a5be1fc8cd9f31a78f4a4dadd63f9089e21b7a02
imliubo <imliubo@makingfun.xyz>
2024-02-26 21:37:31 +0800

    Supports M5 Core, Core2, CoreS3 development boards

--- src/user_app.cpp
+++ src/user_app.cpp
@@ -1,11 +1,39 @@
 #include "lvgl.h"
+#include "lvgl_port_m5stack.hpp"
 #include "demos/lv_demos.h"
 
 void user_app(void) {
+    // You can test the lvgl default demo
+    //*
+    if (lvgl_port_lock()) {
 #if LVGL_USE_V8 == 1
-    lv_demo_widgets();
+        lv_demo_widgets();
 #elif LVGL_USE_V9 == 1
-    // lv_demo_widgets();  // lv_demo_widgets not work with v9 now
-    lv_demo_stress();
+        // lv_demo_widgets not work proerly with v9 for now :(
+#if defined(ARDUINO) && defined(ESP_PLATFORM)
+        lv_demo_benchmark();
+#else
+        // lv_demo_stress not work properly with actual devices for now :(
+        lv_demo_stress();
 #endif
+#endif
+        lvgl_port_unlock();
+    }
+    //*/
+
+    // Or you can create your own lvgl app here
+    /*
+    if (lvgl_port_lock()) {
+        static lv_obj_t* label;
+
+        static lv_obj_t* btn1 = lv_btn_create(lv_scr_act());
+        lv_obj_align(btn1, LV_ALIGN_CENTER, 0, 0);
+
+        label = lv_label_create(btn1);
+        lv_label_set_text(label, "Button");
+        lv_obj_center(label);
+
+        lvgl_port_unlock();
+    }
+    //*/
 }
